152208cc8a8e3efb3d6d232947f708bf490e5dd4
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-04-29 09:40:45 +0200

    hasta aqui hemos llegado

--- 2-WEB-STORAGE/Ejercicio/main.js
+++ 2-WEB-STORAGE/Ejercicio/main.js
@@ -4,6 +4,8 @@ const submitButton = document.querySelector("#submit");
 console.log(submitButton);
 const clearButton = document.querySelector("#reset");
 console.log(clearButton);
+const userList = document.querySelector(".new-users")
+console.log(userList)
 
 // 2.creamos el evento del boton que llama la funcion
 submitButton.addEventListener("click", getUser);
@@ -11,13 +13,14 @@ clearButton.addEventListener("click",deteleAllUsers);
 
 // 3.creamos el array donde guardaremos la info
 let users =[]
+console.log(users,"arr usuarios inicial")
 // 4.funcion que recopile datos del formulario.
 function getUser(item) {
     item.preventDefault()
   // 5.recoger datos de los inputs del formulario
-  let getName = document.querySelector("#name").value;
-  let getEmail = document.querySelector("#email").value;
-  let getMessage = document.querySelector("#message").value;
+  let getName = document.querySelector("#name").value
+  let getEmail = document.querySelector("#email").value
+  let getMessage = document.querySelector("#message").value
   console.log(getName, getEmail, getMessage);
 
   // 6.creamos el objeto donde se guardará la info del formulario
@@ -27,20 +30,55 @@ function getUser(item) {
     message: getMessage,
   };
 
-  console.log(userData)
+  console.log(userData," obj nuevo usuario")
+  console.log(users,users.length +1 ,"arr total usuarios")
 
   // 7.pushea en el array los valores del objeto
   users.push(userData);
-//   8.almacena en local el array como cadena JSON
-  localStorage.setItem("stringUsers", JSON.stringify(users));
-//   9.Muestra por consola la informacion del DOM
-  let infoUser = JSON.parse(localStorage.getItem("stringUsers",users))
+  //   8.almacena en local el array como cadena JSON
+localStorage.setItem("stringUsers", JSON.stringify(users));
+userList.innerHTML = ``;
+  printUsers()
+ 
+}
+
+function printUsers(){
+  //   9.Muestra por consola la informacion del DOM
+  let infoUser = JSON.parse(localStorage.getItem("stringUsers",users));
 console.log(infoUser)
+
+for(let i = 0; i < infoUser.length; i++){
+
+  userList.innerHTML+= `
+                        <p>${infoUser[i].name}</p> <p>${infoUser[i].email}  </p>
+                        <button onclick = "borrame('${infoUser[i].name}')">Borrar usuario</button>
+                        `
+                        // creamos en html el boton para borrar. Este boton incluye el evento onclick que llama a la funcion directamente. En esta funcion entra el valor(del nombre en este caso) asi que cuando la llame también hará falta que algo entre tambien
+              
 }
-
-// 10. Funcion borrar usuarios.
-//  esto me esta borrando el localStorage, pero al añadir otro recupera la parte anterior, no se reinicia
-
+  
+}
 function deteleAllUsers(){
-    localStorage.clear()
+
+  // https://es.stackoverflow.com/questions/103670/c%C3%B3mo-vaciar-un-array-en-javascript
+
+  // limpiar localStorage
+localStorage.clear()
+// limpiar el array
+while(users.length > 0)
+ users.pop(); 
+
+ console.log(users,"todos borrados")
+// limpiar la pantalla 
+ userList.innerHTML = ``;
 }
+
+function borrame (nombre) {
+  // a esta funcion le entra de antes un valor, asi que ahora debe entrarle algo tambien. El nombre de ese algo da igual,pero podré usarlo dentro de la funcion como quiera.
+  console.log(nombre)
+}
+
+
+// Ahora al imprimir por pantalla me identifica qué usuario es con la funcion borrame,lo que necesito ahora es hacer un bucle que me recorra el Array y pare donde coincida ese valor con el usuario
+
+//  Ahora ese objeto del array deberá ser eliminado del array original, porque hemos borrado el usuario. El nuevo array sin el usuario eliminado deberá guardarse en localStorage de nuevo, ya que pintamos en pantalla por esa vía. Después habrá que volver a imprimir por pantalla el nuevo array (sin el elemento borrado).
