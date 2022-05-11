ae22c844a57c90dd92a11229d8d039ffdc049912
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-05-11 22:54:14 +0200

    ejercicio github casi terminado

--- 6-ASINCRONIA-AXIOS/ejercicio-opcional-github/main.js
+++ 6-ASINCRONIA-AXIOS/ejercicio-opcional-github/main.js
@@ -1,11 +1,61 @@
+
+const container = document.getElementById("container")
 const form = document.getElementById("form")
-const example = document.getElementById("example")
-const perfil =document.getElementById("perfil")
+const search = document.getElementById("search")
+const profile = document.getElementById("profile")
+const pic = document.getElementById("pic")
+const username = document.getElementById("username")
+const link = document.getElementById("link")
+const repo = document.getElementById("repo")
+const btn = document.getElementById("btn")
+const API_URL = " https://api.github.com/users/";
 
 
 
-myFunction = () =>{
-    perfil.className = 'card mx-auto visible'
+
+showUser = () =>{
+    profile.className = 'card mx-auto visible'
 
 }
-example.addEventListener("click", myFunction)
+printPicture = (pictureLink) =>{
+pic.src = pictureLink
+}
+printLogin = (gitHubId) =>{
+username.innerText = gitHubId
+}
+printRepo = (repos)=>{
+repo.innerText = `${repos} repositories`
+}
+printUrl = (url) =>{
+link.href= url 
+}
+const searchUser = (e) => {
+    e.preventDefault();
+    let user = search.value;
+    let userUrl = API_URL+user;
+    console.log(userUrl)
+    search.value = ""
+    axios
+      .get(userUrl)
+      .then((res) => {
+        const {public_repos, login, avatar_url,html_url } = res.data
+        console.log(res.data)
+        console.log(public_repos, login, avatar_url,html_url)
+        
+        printPicture(avatar_url)
+        printLogin(login)
+        printRepo(public_repos)
+        printUrl(html_url)
+        showUser()
+      })
+      .catch((err) => printLogin(no_encontrado));
+  };
+
+
+
+
+
+
+
+
+btn.addEventListener("click", searchUser)
