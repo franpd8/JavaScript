c7a20f6a46099d1fe050c36328f5cef0a00bc59c
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-05-15 20:28:14 +0200

    pokm bueno casi listo

--- 7-ASYNC-AWAIT/ejercicio-pokemon/main.js
+++ 7-ASYNC-AWAIT/ejercicio-pokemon/main.js
@@ -4,65 +4,88 @@ const form = document.getElementById("form")
 const search = document.getElementById("search")
 const profile = document.getElementById("profile")
 const pic = document.getElementById("pic")
-const username = document.getElementById("username")
+const number = document.getElementById("number")
 const link = document.getElementById("link")
-const repo = document.getElementById("repo")
 const btn = document.getElementById("btn")
+const type = document.getElementById("type")
 const API_URL = "https://pokeapi.co/api/v2/pokemon/";
 
 const imagen404 = "./assets/404.png"
 
 
 
-showUser = () =>{
+showPkm = () =>{
     profile.className = 'card mx-auto visible'
-
 }
 printPicture = (pictureLink) =>{
 pic.src = pictureLink
 }
-printLogin = (gitHubId) =>{
-username.innerText = gitHubId
+printNumber = (pkmID,pkmName) =>{
+const capitalizedName = pkmName.charAt(0).toUpperCase()+ pkmName.slice(1)
+
+number.innerHTML = `
+<h6>#${pkmID}</h6 <br>
+${capitalizedName}
+` 
 }
-printRepo = (repos)=>{
-repo.innerText = `${repos} repositories`
+printType = (pkmTypesData)=>{
+  // console.log(pkmTypesData)
+  type.innerHTML = ``
+  pkmTypesData.forEach(pkmType =>{
+    console.log(pkmType.type.name)
+    // develop a switch for next version
+    if(pkmType.type.name == 'fire' ){
+      type.innerHTML += `
+      <div class="btn btn-danger"> 
+      ${pkmType.type.name} 
+      </div>`
+    }else{
+      type.innerHTML += `
+      <div class="btn btn-success"> 
+      ${pkmType.type.name} 
+      </div>`
+    }
+  })
+
+
 }
-printUrl = (url) =>{
-link.href= url 
-}
+
 
 printError = () =>{
     printPicture(imagen404)
-    printLogin("User not Found")
-    printRepo("Sorry")
-    printUrl()
+    printNumber("Pokemon not Found")
+    // printRepo("Sorry")
+    // printUrl()
 
 }
-const searchUser = (e) => {
-    e.preventDefault();
-    let user = search.value;
-    let userUrl = API_URL+user;
-    console.log(userUrl)
-    search.value = ""
-    axios
-      .get(userUrl)
-      .then((res) => {
-        const {id, sprites, types} = res.data
-        console.log(types)
-        // console.log(public_repos, login, avatar_url,html_url)
-        
-        printPicture(avatar_url)
-        printLogin(login)
-        printRepo(public_repos)
-        printUrl(html_url)
-        showUser()
-      })
 
-      .catch((err) => {
-        console.error(err)
-        printError()
+const searchPkm = async(e) => {
+    e.preventDefault();
+    let pkmName = search.value;
+    let pkmUrl = API_URL+pkmName;
+    console.log(pkmUrl)
+    search.value = ""
+
+    try{
+      const res = await axios.get(pkmUrl)
+      const pokemon = res.data
+        const {id, sprites, types,name} = pokemon
+        
+
+        // console.log(id)
+        // console.log(pokemon)
+        // console.log(sprites.front_default)
+        
+        printPicture(sprites.front_default)
+        printNumber(id,name)
+        printType(types)
+        // printUrl(html_url)
+        showPkm()
+    }
+    catch(error){
+        // printError()
       } 
-      );
+  
   };
 
 
@@ -72,4 +95,5 @@ const searchUser = (e) => {
 
 
 
-btn.addEventListener("click", searchUser)
+form.addEventListener("submit", searchPkm)
+btn.addEventListener("click", searchPkm)
